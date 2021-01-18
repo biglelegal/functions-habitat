a6c4e20ae47b7bafb9383287cdd2836814142f3f
dtomasbar <daniel.tomas@biglelegal.com>
2021-01-18 22:45:29 +0100

    Remove lib from git

--- functions/lib/app/entities/getUrlResponse.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GetUrlResponse = void 0;
-class GetUrlResponse {
-}
-exports.GetUrlResponse = GetUrlResponse;
-;
-//# sourceMappingURL=getUrlResponse.js.map
--- functions/lib/app/entities/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var corsOptions_1 = require("./corsOptions");
-Object.defineProperty(exports, "corsOptions", { enumerable: true, get: function () { return corsOptions_1.corsOptions; } });
-var functionResponse_1 = require("./functionResponse");
-Object.defineProperty(exports, "FunctionResponse", { enumerable: true, get: function () { return functionResponse_1.FunctionResponse; } });
-var user_1 = require("./user");
-Object.defineProperty(exports, "User", { enumerable: true, get: function () { return user_1.User; } });
-//# sourceMappingURL=index.js.map
